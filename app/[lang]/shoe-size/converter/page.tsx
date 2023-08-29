b5070a79b2123d5c7e1c3ba5a377ec18b75f8520
Filipp Loss <filipploss@MacBook-Pro-2.local>
2023-08-29 20:04:26 +0400

    wip: metadata

--- app/[lang]/shoe-size/converter/page.tsx
+++ app/[lang]/shoe-size/converter/page.tsx
@@ -2,6 +2,12 @@ import { Locale } from "@/i18n.config";
 import { getDictionary } from "../../../lib/dictionary";
 import Converter from "./Converter";
 
+export const metadata = {
+  title: "Find Your Perfect Fit: The Shoe Size Converter",
+  description:
+    "Navigate the world of online shopping effortlessly with our shoe size converter, ensuring you never experience 'too tight' or 'too loose' again.",
+};
+
 export default async function Page({
   params: { lang },
 }: {
