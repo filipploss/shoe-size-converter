47702e6c5779701f37cc40dc1fc7996a2e90ca52
Filipp Loss <filipploss@MacBook-Pro-2.local>
2023-08-30 21:42:57 +0400

    gtm

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 import { Locale } from "@/i18n.config";
 import Box from "@mui/material/Box";
 import { ThemeProvider } from "@mui/material/styles";
+import Script from "next/script";
 import { theme } from "../[lang]/theme";
 import "./globals.css";
 
@@ -19,8 +20,24 @@ export default function RootLayout({
   return (
     <ThemeProvider theme={theme}>
       <html lang={lang}>
+        <head>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WCS9WXQD');        
+          `}
+          </Script>
+        </head>
         <body>
-          <Header lang={lang}/>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html:
+                '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCS9WXQD" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+            }}
+          />
+          <Header lang={lang} />
           <Breadcrumbs />
           <Box
             alignItems="center"
