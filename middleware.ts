4a7b7dff5f287f5d225ea40789c184d6f4119a2d
Filipp Loss <filipploss@MacBook-Pro-2.local>
2023-08-31 13:17:47 +0400

    robots.txt

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)']
 }
