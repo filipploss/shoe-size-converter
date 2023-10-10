34a53bc989c5224eaa5241eeae2ed44144bf5c21
Filipp Loss <filipploss@MacBook-Pro-2.local>
2023-10-10 12:19:52 +0400

    commented code remove

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
-const withNextIntl = require("next-intl/plugin")(
-  // This is the default (also the `src` folder is supported out of the box)
-  "./i18n.ts"
-);
+const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
-module.exports = withNextIntl({
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/es/talla-de-zapato",
-  //       destination: "/shoe-size", // The :path parameter isn't used here so will be automatically passed in the query
-  //     },
-  //     {
-  //       source: "/es/talla-de-zapato/conversor",
-  //       destination: "/shoe-size", // The :path parameter isn't used here so will be automatically passed in the query
-  //     },
-  //   ];
-  // },
-  // pathnames: {
-  //   "/shoe-size": {
-  //     es: "/talla-de-zapato",
-  //   },
-  // },
-  // Other Next.js configuration ...
-});
+module.exports = withNextIntl({});
